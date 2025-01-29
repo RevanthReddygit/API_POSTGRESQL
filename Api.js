bba9d32ace3394e68c5d1a04dc5be562303ea652
RevanthReddygit <kvsrr0306@gmail.com>
2025-01-29 12:48:11 +0530

    Update Api.js

--- Api.js
+++ Api.js
@@ -10,67 +10,75 @@ app.listen(3300, ()=>{
 
 client.connect();
 
-app.get('/users', (req, res)=>{
-    client.query(`Select * from users`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
+app.get('/users', async (req, res)=>{
+    try{
+        await client.query(`Select * from users`), 
+        res.send(result.rows);   
+    }catch (err){
+        console.error(err.message)
+    }finally{
+        client.end;
+    }
         }
-    });
-    client.end;
-})
-
-app.get('/users/:id', (req, res)=>{
-    client.query(`Select * from users where id=${req.params.id}`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-})
+    );
+app.get('/users/:id', async (req, res)=>{
+    try{
+        await client.query(`Select * from users where id=${req.params.id}`)
+                res.send(result.rows);
+    }catch(err){
+        console.error(err.message)
+    }finally{
+        client.end;
+    }
+ });
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.post('/users', (req, res)=> {
+app.post('/users', async (req, res)=> {
     const user = req.body;
-    console.log(user)
-    let insertQuery = `insert into users(id, firstname, lastname, location) 
+    try{
+        console.log(user)
+    const insertQuery = `insert into users(id, firstname, lastname, location) 
                        values(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`
-
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Insertion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
+                       await client.query(insertQuery);
+                            
+                              res.send('Insertion was successful');
+    }catch(err){
+        console.error(err.message)
+    }finally{
+        client.end;
+    }
 })
 
-app.put('/users/:id', (req, res)=> {
-    let user = req.body;
-    let updateQuery = `update users
+app.put('/users/:id', async (req, res)=> {
+     try{
+
+        const user = req.body;
+    const updateQuery = `update users
                        set firstname = '${user.firstname}',
                        lastname = '${user.lastname}',
                        location = '${user.location}'
-                       where id = ${user.id}`
-
-    client.query(updateQuery, (err, result)=>{
-        if(!err){
-            res.send('Update was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
+                       where id = ${user.id}`;
+                      await  client.query(updateQuery);
+                         
+                            res.send('Update was successful');
+        
+     }catch (err){
+      console.error(err.message)
+     }finally{
+        client.end;
+     }
 })
 
-app.delete('/users/:id', (req, res)=> {
-    let insertQuery = `delete from users where id=${req.params.id}`
-
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Deletion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
-})
+app.delete('/users/:id', async (req, res)=> {
+    try{
+        const insertQuery = `delete from users where id=${req.params.id}`
+       await client.query(insertQuery), 
+                res.send('Deletion was successful');
+    }catch(err){
+        console.error(err.message)
+    }finally{
+        client.end;
+    }
+ })
